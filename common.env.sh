1ffcdf7d73ecd9ef4f1e88220b0fd5aa0f5db389
cogs <cogs@springless.com>
2023-01-03 08:52:04 -0700

    Add alias for kitty terminal SSH

--- common.env.sh
+++ common.env.sh
@@ -8,3 +8,10 @@ GO_VERSION=1.19.4
 GO_DIR="${MY_APP_DIR}/install/go/${GO_VERSION}"
 
 export PATH=${GO_DIR}/bin:${PATH}
+
+alias dc=docker-compose
+
+# Kitty has some terminal issues while SSHing; this kitten attempts to resolve that automatically
+# and copies over some terminfo to the remote host
+# See: https://wiki.archlinux.org/title/Kitty#Terminal_issues_with_SSH
+[ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
