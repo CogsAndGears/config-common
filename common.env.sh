82decedddac4385ef506e5e3c06ed3708de8224b
cogs <cogs@springless.com>
2023-11-14 06:47:07 -0600

    Update go version, add Android SDK to path

--- common.env.sh
+++ common.env.sh
@@ -4,7 +4,7 @@ if [ -z "${MY_APP_DIR}" ]; then
 fi
 
 # Add go
-GO_VERSION=1.19.4
+GO_VERSION=1.21.4
 GO_DIR="${MY_APP_DIR}/install/go/${GO_VERSION}"
 
 export PATH=${GO_DIR}/bin:${PATH}
@@ -15,3 +15,8 @@ alias dc=docker-compose
 # and copies over some terminfo to the remote host
 # See: https://wiki.archlinux.org/title/Kitty#Terminal_issues_with_SSH
 [ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
+
+# Add Android SDK to path
+export PATH=$PATH:${MY_APP_DIR}/install/android-studio/bin
+alias android-studio="pushd ${MY_APP_DIR}/install/android-studio/bin && ./studio.sh && popd"
+alias adb="~/Android/Sdk/platform-tools/adb"
