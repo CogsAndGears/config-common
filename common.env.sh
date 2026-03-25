0f0ddc833bb721853b640b198c35ac4e7610e909
cogs <cogs@springless.com>
2026-03-25 09:28:13 -0500

    Update common environment

--- common.env.sh
+++ common.env.sh
@@ -3,9 +3,11 @@ if [ -z "${MY_APP_DIR}" ]; then
   return 1
 fi
 
+# Commented out for now since the official installer is probably a bit easier to
+# manage
 # Add go
-GO_VERSION=1.21.4
-GO_DIR="${MY_APP_DIR}/install/go/${GO_VERSION}"
+#GO_VERSION=1.21.4
+#GO_DIR="${MY_APP_DIR}/install/go/${GO_VERSION}"
 
 export PATH=${GO_DIR}/bin:${PATH}
 
@@ -28,3 +30,10 @@ export PATH=$PATH:${MY_APP_DIR}/install/obsidian/installed
 export PATH=$PATH:/srv/app/install/texlive/2024/bin/x86_64-linux
 export INFOPATH=$INFOPATH:/srv/app/install/texlive/2024/texmf-dist/doc/info
 export MANPATH=$MANPATH:/srv/app/install/texlive/2024/texmf-dist/doc/man
+
+# Add Ruby to path
+export PATH=$PATH:/srv/app/install/ruby/installed/bin
+
+# Add manual Postgres build to path
+export PATH=$PATH:/srv/app/install/postgresql/installed/bin
+
